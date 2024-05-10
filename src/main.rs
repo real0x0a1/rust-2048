ea05e90f3cabda24f7f0bbf570190a05a988f5d3
real0x0a1 <143189904+real0x0a1@users.noreply.github.com>
2024-05-10 20:34:57 +0330

    Updated src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -17,6 +17,19 @@ impl Game {
             grid: [[0; GRID_SIZE]; GRID_SIZE],
             score: 0,
         };
+        game.add_tile();
+        game.add_tile();
         game
+    }
+
+    fn add_tile(&mut self) {
+        let mut rng = rand::thread_rng();
+        let mut row = rng.gen_range(0..GRID_SIZE);
+        let mut col = rng.gen_range(0..GRID_SIZE);
+        while self.grid[row][col]!= 0 {
+            row = rng.gen_range(0..GRID_SIZE);
+            col = rng.gen_range(0..GRID_SIZE);
+        }
+        self.grid[row][col] = 2;
     }
 }
